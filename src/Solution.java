95c783862d4f8b770f4ab27b1d9d34f8a6c207a7
Minin <minin1506@gmail.com>
2019-06-27 00:43:25 +0300

    eng

--- src/Solution.java
+++ src/Solution.java
@@ -9,32 +9,32 @@ public class Solution {
         a = Math.random()*200-100;
         b = Math.random()*200-100;
         c = Math.random()*200-100;
-        System.out.println(" Случайная переменная а = " +a);
-        System.out.println(" Случайная переменная b = " +b);
-        System.out.println(" Случайная переменная c = " +c);
+        System.out.println(" Random value for а = " +a);
+        System.out.println(" Random value for b = " +b);
+        System.out.println(" Random value for c = " +c);
 
 
         // Code for user's values
 
-        //System.out.println("Введите a, b и с для уравнения: ax^2 +bx +c =0 ");
+        //System.out.println("Enter your values ​​for a, b и с variables : ax^2 +bx +c =0 ");
         //Scanner in = new Scanner(System.in);
         //a = in.nextDouble();
         //b = in.nextDouble();
         //c = in.nextDouble();
 
         d = b * b - 4 * a * c;
-        System.out.println(" Дискриминант = " +d);
+        System.out.println(" Discriminant = " +d);
         if (d > 0) {
             double x1, x2;
             x1 = (-b - Math.sqrt(d)) / (2 * a);
             x2 = (-b + Math.sqrt(d)) / (2 * a);
-            System.out.println("1й корень= " + x1 + "  2й корень= " + x2);
+            System.out.println("1st root= " + x1 + "  2nd root= " + x2);
         } else if (d == 0) {
             double x;
             x = -b / (2 * a);
-            System.out.println("Корень =" + x);
+            System.out.println("Root =" + x);
         } else {
-            System.out.println("Нет корней/ ");
+            System.out.println("No solutions ");
         }
 
     }
